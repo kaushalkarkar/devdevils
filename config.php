0c20059e7c9c38a8828a513f3f63659a065e8688
Karkar Kaushal <87358709+kaushalkarkar@users.noreply.github.com>
2022-04-01 22:31:56 +0530

    avyam
    dev devils

---
+++ config.php
@@ -0,0 +1,18 @@
+ <?php
+
+$keyId = 'rzp_test_KpssrqiIUVbWEo';
+$keySecret = 'VCY0uHNCskUvsUm19iMTqufS';
+$displayCurrency = 'INR';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+//DATABASE CONNECTION DETAILS
+$host = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "payment";
---
+++ paymentnav.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    
+    <!-- Bootstrap CSS -->
+    
+        <link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
+
+    
+  
+    
+    
+
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans&display=swap" rel="stylesheet">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+  <a class="navbar-brand" href="#"><i class="fa fa-user-plus" aria-hidden="true"></i> Global Hospital </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <style >
+    .bg-primary {
+    background: -webkit-linear-gradient(left, #3931af, #00c6ff);
+}
+.list-group-item.active {
+    z-index: 2;
+    color: #fff;
+    background-color: #342ac1;
+    border-color: #007bff;
+}
+.text-primary {
+    color: #342ac1!important;
+}
+
+.btn-primary{
+  background-color: #3c50c1;
+  border-color: #3c50c1;
+}
+
+h3 {
+  text-align: center;
+  text-transform: uppercase;
+  color: #4CAF50;
+  padding: 8px;
+}
+
+
+form {
+  background-color: white;
+  width: 500px;
+  border: 1px solid black;
+  padding: 25px;
+  margin: 20px;
+}
+
+
+
+
+  </style>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+     <ul class="navbar-nav mr-auto">
+       <li class="nav-item">
+        <a class="nav-link" href="logout.php"><i class="fa fa-sign-out" aria-hidden="true"></i>Logout</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#"></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+  </head>
+  <style type="text/css">
+    button:hover{cursor:pointer;}
+    #inputbtn:hover{cursor:pointer;}
+  </style>
+  <body style="padding-top:50px;">
+  
+   <div class="container-fluid" style="margin-top:50px;">
+    <!-- <h3 style = "margin-left: 40%;  padding-bottom: 20px; font-family: 'IBM Plex Sans', sans-serif;"> Welcome &nbsp<?php echo $username ?> 
+   </h3> -->
+    <div class="row">
+  <div class="col-md-4" style="max-width:25%; margin-top: 3%">
+    <div class="list-group" id="list-tab" role="tablist">
+      <a class="list-group-item list-group-item-action active" id="list-dash-list">Dashboard</a>
+      <a class="list-group-item list-group-item-action" href="admin-panel.php" >Book Appointment</a>
+      <a class="list-group-item list-group-item-action" href="admin-panel.php" >Payment</a>
+                                                                                                                                   
+      <a class="list-group-item list-group-item-action" href="admin-panel.php" >Appointment History</a>
+      <a class="list-group-item list-group-item-action" href="admin-panel.php" >Prescriptions</a>
+      
+    </div><br>
+  </div>
+
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Payment Form</title>
+</head>
+<body>
+    
+    <br>
+
+    <form action="pay.php" method="post">
+        <br>
+        <br>
+
+        <h3>Payment Details</h3>
+        Price: <input type="text" name="price"><br>
+        <br>
+        Name: <input type="text" name="customername"><br>
+        <br>
+        Email: <input type="email" name="email"><br>
+        <br>
+        Contact No: <input type="text" name="contactno"><br>
+        <br>
+        <input type="submit" name="submit" value="Proceed To Pay" class="btn btn-primary">
+        
+
+       
+
+
+
+
+
+
+
+
+
+
+</form>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.10.1/sweetalert2.all.min.js">
+   </script>
+
+
+
+  </body>
+</html>
